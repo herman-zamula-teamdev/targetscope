7424332d3ceccdc792bbb8f04fee5b44dcd0fa24
Alex Reynolds <alexpreynolds@gmail.com>
2015-01-19 22:23:35 -0800

    added changes

--- source/conf.py
+++ source/conf.py
@@ -119,10 +119,10 @@ html_theme_options = {
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-html_title = "targetscope"
+html_title = "Targetscope API"
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-html_short_title = "Entity and request API"
+#html_short_title = "Entity and request API"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
