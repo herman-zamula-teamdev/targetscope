5dd3e217069fc3f46352635ed09c9badc6c51436
Alex Reynolds <alexpreynolds@gmail.com>
2015-01-19 21:03:17 -0800

    testing CSS change

--- source/conf.py
+++ source/conf.py
@@ -111,7 +111,7 @@ html_theme = 'better'
 # further.  For a list of options available for each theme, see the
 # documentation.
 html_theme_options = {
-    'cssfiles': ['_static/custom_style.css']
+    'cssfiles': ['../_static/custom_style.css']
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
